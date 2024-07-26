6063dea7b1f058543e190ad03442f284627baefd
zhoucy <zhoucy@tydic.com>
2024-07-26 22:23:58 +0800

    rocketmq单机部署

---
+++ rocketmq-standalone/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#将ip地址更换为本机ip地址
+BROKERIP1=192.168.10.129
+rm -rf conf/broker.conf
+cp conf/broker.conf.temp conf/broker.conf
+sed -i "s#BROKERIP1#${BROKERIP1}#g" conf/broker.conf
+mkdir -p ./data/broker/logs
+mkdir -p ./data/broker/data
+mkdir -p ./data/nameserver/logs
+chmod 777 -R ./data
+docker compose -f docker-compose.yaml up -d
