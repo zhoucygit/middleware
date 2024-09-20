3d3053f477f10809053c3493c02c2996267142db
zhoucy <zhoucy@tydic.com>
2024-09-20 16:24:54 +0800

    1

--- redis-cluster/initcluster.sh
+++ redis-cluster/initcluster.sh
@@ -2,4 +2,6 @@
 host1=192.168.10.210
 host2=192.168.10.230
 host3=192.168.10.213
-redis-cli --cluster create ${host1}:7001 ${host2}:7001 ${host3}:7001 ${host1}:7002 ${host2}:7002 ${host3}:7002 --cluster-replicas 1
+redisport1=7001
+redisport2=7002
+docker exec -it redis-7001 redis-${redisport1} --cluster create ${host1}:${redisport1} ${host2}:${redisport1} ${host3}:${redisport1} ${host1}:${redisport2} ${host2}:${redisport2} ${host3}:${redisport2} --cluster-replicas 1 -a Redis235
