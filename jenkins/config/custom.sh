2674dba871fe4952713ba4a2417f9339d3b764ef
zhoucy <zhoucy@tydic.com>
2024-11-12 18:08:30 +0800

    1

---
+++ jenkins/config/custom.sh
@@ -0,0 +1,6 @@
+export  NODE_HOME=/middleware/node
+export  PATH=$NODE_HOME/bin:$PATH
+export  MVN_HOME=/middleware/maven
+export  PATH=$MVN_HOME/bin:$PATH
+export JAVA_HOME=/usr/local/jdk
+export  PATH=$JAVA_HOME/bin:$PATH
